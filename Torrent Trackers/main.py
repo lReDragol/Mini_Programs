6e8aa581d7cb4d0090e258ccdf36dd8b6757d89d
Drago <136778548+lReDragol@users.noreply.github.com>
2023-07-16 15:11:18 +0300

    Add files via upload

--- Torrent Trackers/main.py
+++ Torrent Trackers/main.py
@@ -2,8 +2,9 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
-from collections import OrderedDict
 from tkinterdnd2 import DND_FILES, TkinterDnD
+
+
 
 def remove_duplicates():
     file_path = file_path_var.get()
@@ -14,11 +15,20 @@ def remove_duplicates():
     with open(file_path, 'r') as file:
         trackers = file.readlines()
 
-    unique_trackers = list(OrderedDict.fromkeys(trackers))
+    unique_trackers = []
+    for tracker in trackers:
+        trimmed_tracker = tracker.strip()
+        if remove_checkbox_var.get():
+            unique_trackers.append(trimmed_tracker)
+            if trimmed_tracker:
+                unique_trackers.append('')
+        elif trimmed_tracker and trimmed_tracker not in unique_trackers:
+            unique_trackers.append(trimmed_tracker)
+
     num_duplicates = len(trackers) - len(unique_trackers)
 
     with open(file_path, 'w') as file:
-        file.writelines(unique_trackers)
+        file.writelines([tracker + '\n' for tracker in unique_trackers])
 
     messagebox.showinfo("Готово", f"Удалено: {num_duplicates} дубликатов")
 
@@ -33,6 +43,7 @@ def on_drop(event):
 # Создание главного окна с поддержкой перетаскивания файлов
 root = TkinterDnD.Tk()
 root.title("Удаление дубликатов трекеров")
+root.geometry('300x250')
 
 # Установка стилей для элементов
 style = ttk.Style()
@@ -51,14 +62,16 @@ style.configure("TEntry",
                 background="white",
                 padding=5)
 
-# Переменная для хранения пути к файлу
+
+# Переменные для хранения пути к файлу и состояния Checkbox
 file_path_var = tk.StringVar()
+remove_checkbox_var = tk.BooleanVar()
 
 # Создание метки и поле для отображения пути к файлу
 file_label = ttk.Label(root, text="Выберите файл:")
 file_label.pack(pady=10)
 
-file_entry = ttk.Entry(root, textvariable=file_path_var, width=50)
+file_entry = ttk.Entry(root, textvariable=file_path_var, width=40)
 file_entry.pack(pady=5)
 
 # Привязка события перетаскивания к полю ввода
@@ -68,6 +81,10 @@ file_entry.dnd_bind('<<Drop>>', on_drop)
 # Создание кнопки для выбора файла
 browse_button = ttk.Button(root, text="Обзор", command=browse_file, style="TButton")
 browse_button.pack(pady=5)
+
+# Создание Checkbox для включения/отключения функции удаления дубликатов
+remove_checkbox = ttk.Checkbutton(root, text="Разделять строки пустотой", variable=remove_checkbox_var)
+remove_checkbox.pack(pady=5)
 
 # Создание кнопки "Удалить дубликаты"
 remove_duplicates_button = ttk.Button(root, text="Удалить дубликаты", command=remove_duplicates, style="TButton")
